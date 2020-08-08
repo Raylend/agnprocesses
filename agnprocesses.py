9912395c88c4efea4f948dedb30d060d34532150
Raylend <podlesnyi.ei14@physics.msu.ru>
2020-08-08 20:42:05 +0300

    derishev_q_function() and derishev() added.

--- agnprocesses.py
+++ agnprocesses.py
@@ -1,9 +1,18 @@
-# This is the main python3 module for agnprocessec package.
+# This is the main python3 file for agnprocesses package.
+"""
+agnprocessec is the package for calculation of various astrophysical
+processes which can occur in active galactic nuclei.
+It includes:
+- synchrotron emission
+- inverse Compton (IC) process
+- and more
+"""
 
-import astropy as ap
+from astropy import units as u
+from astropy import constants as const
 import numpy as np
-# from synchro.process import test as t
 import synchro.process
 
 if __name__ == '__main__':
     synchro.process.test()
+    print(1.0 * u.G)
--- synchro/process.py
+++ synchro/process.py
@@ -1,3 +1,36 @@
+"""
+This program computes synchrotron emission from energetic particles
+according to Derishev & Aharonian (2019)
+https://doi.org/10.3847/1538-4357/ab536a
+"""
+
+from astropy import units as u
+from astropy import constants as const
+import numpy as np
+
+def derishev_q_function(x):
+    """
+    This is Derishev's Q_{PL} function
+    see his eq. (10) and (41)
+    """
+    return (1.0 + x**(-2.0/3.0)) * np.exp(-2.0 * x**(2.0/3.0))
+
+def derishev(nu, en, b = 1.0 * u.G):
+    try:
+        g = en.to('eV') / (const.m_e.cgs * const.c.cgs**2).to('eV')
+    except AttributeError:
+        print("energy must be associated with an astropy unit")
+        raise AttributeError
+    omega_0 = 4.0/3.0 * g**2 * const.e.cgs * b / (const.m_e.cgs * const.c.cgs)
+    nu_0 = nu / nu_0
+    return (const.alpha / (3.0 * g**2) * derishev_q_function(x))
+
+
+
+
+
+
+
 def test():
     print("Test completed successfully.")
     return None
