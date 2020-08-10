66c439c4132a5d1e4f8dbfe606d7266924753dc0
Raylend <podlesnyi.ei14@physics.msu.ru>
2020-08-10 15:43:13 +0300

    e_ref is replaced by en_break in spec.broken_power_law()

--- processes/spectra.py
+++ processes/spectra.py
@@ -6,14 +6,13 @@ from astropy import units as u
 from astropy import constants as const
 import numpy as np
 
-def broken_power_law(en, gamma1, gamma2, en_break,
-norm = 1.0 * u.eV**(-1), e_ref = 1.0 * u.eV):
+def broken_power_law(en, gamma1, gamma2, en_break, norm = 1.0 * u.eV**(-1)):
 
     en1 = en[en <= en_break]
     en2 = en[en >  en_break]
 
-    f1 = (en1 / e_ref)**(-gamma1)
-    f2 = (en2 / e_ref)**(-gamma2)
+    f1 = (en1 / en_break)**(-gamma1)
+    f2 = (en2 / en_break)**(-gamma2)
     f = np.concatenate((f1, f2), axis = 0) * norm
 
     return f
