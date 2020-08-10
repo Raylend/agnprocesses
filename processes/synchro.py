0b2060294fcb69e4fae252a582df4e7b9309ba26
Raylend <podlesnyi.ei14@physics.msu.ru>
2020-08-10 16:49:31 +0300

    derishev_synchro_spec() updated. derishev() now can be used both with Lorentz factors and energies.

--- processes/synchro.py
+++ processes/synchro.py
@@ -22,19 +22,37 @@ def derishev_q_function(x):
     """
     return (1.0 + x**(-2.0/3.0)) * np.exp(-2.0 * x**(2.0/3.0))
 
-def derishev(nu, en, b = 1.0 * u.G):
-    try:
-        g = en.to('eV') / (const.m_e.cgs * const.c.cgs**2).to('eV')
-    except AttributeError:
-        raise AttributeError("energy must be associated with an astropy unit")
-    omega_0 = 4.0/3.0 * g**2 * const.e.cgs * b.cgs / (const.m_e.cgs * const.c.cgs)
+def derishev(
+nu, en, b = 1.0 * u.G,
+particle_mass = const.m_e.cgs,
+particle_charge = const.e.gauss
+):
+    list_of_energies = ['J', 'erg', 'eV', 'keV', 'MeV', 'GeV', 'TeV', 'PeV']
+    ############################################################################
+    if type(en) == type(3.0) or type(en) == type(1) \
+     or type(en) == type(np.arange(0,3)):
+        g = en
+    ############################################################################
+    elif en.unit == 'dimensionless':
+        g = en
+    ############################################################################
+    elif en.unit in list_of_energies:
+        g = en / (particle_mass * const.c.cgs**2).to(en.unit)
+    ############################################################################
+    else:
+        raise ValueError("invalid type of the argument 'en'")
+    ############################################################################
+    omega_0 = 4.0/3.0 * g**2 * \
+     particle_charge * b.cgs / (particle_mass * const.c.cgs)
     nu_0 = nu / nu_0
     f = const.alpha / (3.0 * g**2) * derishev_q_function(x)
+    f = (f / const.h * 2.0 * np.pi).to('1/eV * 1/s')
     if any(f.value[f.value <= 0]):
-        raise ArithmeticError("Arithmetic exception in derishev(...) function")
+        raise ArithmeticError("Arithmetic exception in derishev() function")
     return f
 
-def derishev_synchro_spec(nu,
+def derishev_synchro_spec(
+nu,
 b = 1.0 * u.G,
 norm = 1.0 * u.eV**(-1),
 spec_law = 'power_law',
@@ -45,8 +63,11 @@ en_exp_cutoff = None,
 en_min = None,
 en_max = None,
 en_mono = None,
-e_ref = 1.0 * u.eV,
-number_of_integration = 1000):
+en_ref = 1.0,
+number_of_integration = 1000,
+particle_mass = const.m_e.cgs,
+particle_charge = const.e.gauss
+):
     """
     nu is the independent variable, frequency
     b is the magnetic field strength
@@ -68,50 +89,57 @@ number_of_integration = 1000):
 
     en_mono is the energy of charged particle for the 'monoenergetic'
 
-    e_ref is reference energy for 'power_law', 'broken_power_law'
-    or 'exponential_cutoff'
+    en_ref is reference energy for 'power_law' or 'exponential_cutoff'
 
-    ALL energies must be astropy quantities, e.g. en_min = 1.0 * u.GeV
+    ALL energies must be with the same units of astropy quantities or must be
+    nd.arrays (in the latter case they will be considered as Lorentz factors)
     """
+    ############################################################################
     valid_spec_laws = ['power_law', 'broken_power_law',
     'exponential_cutoff', 'monoenergetic']
     if spec_law not in valid_spec_laws:
         raise ValueError("Invalid spec_law. It must be one of {}"
         .format(valid_spec_laws))
-
     f = None
+    ############################################################################
     if spec_law == 'monoenergetic':
         if any([norm, en_mono]):
-            f = derishev(nu, e_mono, b = b) * norm
+            f = derishev(
+            nu, en_mono, b = b,
+            particle_mass = particle_mass,
+            particle_charge = particle_charge) * norm
         else:
             raise ValueError(
-            "e_mono should be an astropy Quantity, e.g. 5*u.GeV"
+            "en_mono should be an astropy Quantity, e.g. 5*u.GeV"
             )
+    ############################################################################
     elif spec_law == 'broken_power_law':
-        if any([gamma1, en_min, en_max, norm]):
-            ee = np.logspace(
+        if any([gamma1, gamma2, en_break, en_min, en_max, norm]):
+            ee = en_min.unit * np.logspace(
             np.log10(en_min.value),
             np.log10(en_max.value),
             number_of_integration
-            ) * en_min.unit
-
+            )
             def underintegral(energy):
                 return(
-                derishev(nu, energy, b = b) * broken_power_law(
-                energy, gamma1, gamma2, en_break,
-                norm = norm, e_ref = e_ref
-                )
+                derishev(
+                nu, energy, b = b,
+                particle_mass = particle_mass,
+                particle_charge = particle_charge) * broken_power_law(
+                energy, gamma1, gamma2, en_break, norm = norm)
                 )
             f = simps(underintegral, ee)
         else:
             raise ValueError(
-            "make sure you defined all of {} correctly".format(
-            [gamma1, en_min, en_max, norm])
+            "Make sure you defined all of {} correctly".format(
+            [gamma1, gamma2, en_break, en_min, en_max, norm])
             )
-
+    ############################################################################
     return f
 
-
+def test():
+    print("synchro.py imported successfully.")
+    return None
 
 
 
