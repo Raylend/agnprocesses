27b063355350153b1c03fa18425978bca096a9fa
Raylend <podlesnyi.ei14@physics.msu.ru>
2020-10-15 22:14:59 +0300

    cosmology.py added

---
+++ processes/cosmology.py
@@ -0,0 +1,21 @@
+from astropy import units as u
+from astropy import constants as const
+from astropy.cosmology import Planck15 as cosmo
+
+
+def luminosity_distance(redshift):
+    return(cosmo.luminosity_distance(redshift))
+
+
+def test():
+    print("cosmology.py imported successfully.")
+    print("We are usint following cosmology:")
+    H = cosmo.H(0)
+    print("Hubble constant = {:f}".format(H))
+    Om = cosmo.Om0
+    print("Omega_0 = {:f}".format(Om))
+    return None
+
+
+if __name__ == '__main__':
+    test()
