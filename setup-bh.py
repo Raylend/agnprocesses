61e2cec2d696459cfba71568d96d7b8eedb0daf0
Raylend <podlesnyi.ei14@physics.msu.ru>
2020-11-06 13:28:16 +0300

    The issue with the C++ libraries installation resolved

--- setup-bh.py
+++ setup-bh.py
@@ -3,12 +3,26 @@ import subprocess
 
 cmd = "echo $LD_LIBRARY_PATH"
 cmdout = subprocess.check_output(cmd, shell=True)[:-1].decode('utf-8')
+agn_path = None
+try:
+    cmdout = cmdout.split(':')
+    for path in cmdout:
+        if 'agnprocesses/bin/shared' in path:
+            agn_path = path
+            break
+except:
+    raise ImportError(
+        "$LD_LIBRARY_PATH is not defined properly. See README for instructions")
+
+if agn_path is None:
+    raise ImportError(
+        "$LD_LIBRARY_PATH is not defined properly. See README for instructions")
 
 module = Extension(
     "bh_ext",
     sources=['processes/c_codes/BHPairProduction/bh.cpp'],
-    # library_dirs=[cmdout],
-    library_dirs=['/home/raylend/Science/agnprocesses/bin/shared'],
+    library_dirs=[agn_path],
+    # library_dirs=['/home/raylend/Science/agnprocesses/bin/shared'],
     libraries=['BetheHeitler']
 )
 
