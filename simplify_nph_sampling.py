c2f66f7126618712fb00cefaba9280ce95a6dba7
Raylend <podlesnyi.ei14@physics.msu.ru>
2022-06-19 00:48:31 +0300

    added continuum and spikes identification

--- simplify_nph_sampling.py
+++ simplify_nph_sampling.py
@@ -76,18 +76,34 @@ def separate_continuum_and_spikes(y_input, x_input,
             "y and x must have a shape, which can be reshaped into one-dimensional shape"
         )
     max_min_ratio = calculate_windowed_max_min_ratio(
-        y, x, std_window_width=std_window_width)
+        y, x, std_window_width=std_window_width
+    )
     if std_window_width % 2 == 1:
-        x = x[sh + 1: -sh]
-        y = y[sh + 1: -sh]
+        x = x[sh + 1: -sh][:-1]
+        y = y[sh + 1: -sh][:-1]
     else:
-        x = x[sh:-sh]
-        y = y[sh:-sh]
+        x = x[sh:-sh][:-1]
+        y = y[sh:-sh][:-1]
     filt_spike = (max_min_ratio > filt_value)
     difference = np.diff(filt_spike)
-    x_starters = x[:-1][difference == True][0::2]
-    x_enders = x[:-1][difference == True][1::2]
-    return (x_starters, x_enders)
+    x_starters = x[difference == True][0::2]
+    x_enders = x[difference == True][1::2]
+    x_continuum = x[difference == False]
+    y_continuum = y[difference == False]
+    if len(x_starters) > len(x_enders):
+        x_starters = x_starters[:-1]
+    if len(x_enders) > len(x_starters):
+        x_enders = x_enders[:-1]
+    x_spikes = np.zeros(x_starters.shape)
+    y_spikes = np.zeros(x_starters.shape)
+    for i in range(0, len(x_starters)):
+        x_spikes[i] = float(x[int((np.argwhere(x_starters[i] == x) +
+                                   np.argwhere(x_enders[i] == x)) / 2)])
+        y_spikes[i] = np.max(
+            y[int(np.argwhere(x_starters[i] == x)):
+              int(np.argwhere(x_enders[i] == x))]
+        )
+    return (x_spikes, y_spikes)
 
 
 std_window_width = 5
@@ -97,14 +113,14 @@ max_min_ratio = calculate_windowed_max_min_ratio(
     std_window_width=std_window_width
 )
 
-x_starters, x_enders = separate_continuum_and_spikes(
+x_spikes, y_spikes = separate_continuum_and_spikes(
     field_numpy[:, 1],
     field_numpy[:, 0],
     std_window_width=std_window_width,
     filt_value=4.0
 )
 
-print("Number of detected high lines = ", len(x_starters))
+print("Number of detected high lines = ", len(x_spikes))
 
 t_end_numpy = test_pytorch.get_current_date_time()
 print("Numpy sampling ended at: ",
@@ -129,10 +145,20 @@ plt.plot(
     label='max_min_ratio'
 )
 
-for i in range(0, len(x_starters)):
-    xx = np.ones((50, )) * x_starters[i]
-    yy = np.logspace(-9, 0, 50)
-    plt.plot(xx, yy, linestyle="-", color="green", linewidth=1)
+for i in range(0, len(x_spikes)):
+    xx = np.ones((50, )) * x_spikes[i]
+    yy = np.logspace(-9, np.log10(y_spikes[i]), 50) / np.max(field_numpy)
+    plt.plot(xx, yy, linestyle="--", color="orange", linewidth=1)
+
+# for i in range(0, len(x_starters)):
+#     xx = np.ones((50, )) * x_starters[i]
+#     yy = np.logspace(-9, 0, 50)
+#     plt.plot(xx, yy, linestyle="-", color="green", linewidth=1)
+#
+# for i in range(0, len(x_enders)):
+#     xx = np.ones((50, )) * x_enders[i]
+#     yy = np.logspace(-9, 0, 50)
+#     plt.plot(xx, yy, linestyle="-", color="orange", linewidth=1)
 
 # plt.xlabel('energy, ', fontsize=18)
 # plt.xticks(fontsize=12)
